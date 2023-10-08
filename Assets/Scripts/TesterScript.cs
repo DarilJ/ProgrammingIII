17578e7a48910251648ce2261e9b873771e57062
Daril Camilo <darilvendas@hotmail.com>
2023-10-07 23:49:54 -0500

    Second Refresh
    I tried a lot to fix the second task but was not possible yet

--- Assets/Scripts/TesterScript.cs
+++ Assets/Scripts/TesterScript.cs
@@ -8,21 +8,30 @@ using UnityEngine.Events;
 public class TesterScript : MonoBehaviour
 
 {
+    //declaring the scriptable object
     public CharacterBase myCharacter;
 
     public Slider healthBar;
     public GameObject damageEffectObject;
     public GameObject healEffectObject;
 
+    /*
+    private AudioSource _audioSource;
+    [SerializeField]
+    private AudioClip healing;
+    [SerializeField]
+    private AudioClip damage;
+    */
     public UnityEvent<float, bool> OnHealthChange = new UnityEvent<float, bool>();
 
     void Start()
     {
+        //_audioSource = GetComponent<AudioSource>();
+
         myCharacter.currentHealth = myCharacter.maxHealth;
         healthBar.value = myCharacter.currentHealth;
 
         OnHealthChange.AddListener(UpdateHealth);
-        OnHealthChange += UpdateHealth;
     }
 
     // Update is called once per frame
@@ -32,7 +41,7 @@ public class TesterScript : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            ApplyChange(-myCharacter.damage, false); // Aplica dano
+            ApplyChange(-myCharacter.damage, false); // The damage is applied
         }
 
         if (Input.GetMouseButtonDown(1))
@@ -40,8 +49,9 @@ public class TesterScript : MonoBehaviour
             ApplyChange(myCharacter.heal, true); // Aplica cura
         }
     }
-    void ApplyChange(float amount, bool isHealing)
+    public void ApplyChange(float amount, bool isHealing)
     {
+        Debug.Log("ApplyChange called");
         myCharacter.currentHealth += amount;
 
         // Certifique-se de que a saúde não ultrapasse os limites
@@ -49,28 +59,35 @@ public class TesterScript : MonoBehaviour
 
         // Notifique os ouvintes com o valor do dano ou cura e se é cura ou dano
         OnHealthChange?.Invoke(amount, isHealing);
+
+        UpdateHealth(amount, isHealing);
     }
     public void UpdateHealth(float amount, bool isHealing)
     {
+        Debug.Log("UpdateHealth called");
         if (isHealing)
         {
             Debug.Log("You were healed by " + amount + " health points");
+            /*
+            _audioSource.clip = healing;
+            _audioSource.Play();
+            */
+            healEffectObject.SetActive(true);
+            //Debug.Log("isHealing = true");
+            healEffectObject.GetComponent<AudioSource>().Play();
+            //Debug.Log("Audio Source On");
+            healEffectObject.GetComponent<ParticleSystem>().Play();
+            //Debug.Log("Particles On");
         }
         else
         {
             Debug.Log("You lost " + amount + " health points");
-        }
-        if (isHealing)
-        {
-            healEffectObject.SetActive(true);
-            healEffectObject.GetComponent<AudioSource>().Play();
-            healEffectObject.GetComponent<ParticleSystem>().Play();
-        }
-        else
-        {
             damageEffectObject.SetActive(true);
+            //Debug.Log("isHealing = false");
             damageEffectObject.GetComponent<AudioSource>().Play();
+            //Debug.Log("Audio Source On");
             damageEffectObject.GetComponent<ParticleSystem>().Play();
+            //Debug.Log("Particles On");
         }
         if (myCharacter.currentHealth <= 0)
         {
